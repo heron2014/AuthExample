ff653d39fd2e80c1d972de2ed3ac26ad83f8102f
Anita <a.nita@hotmail.co.uk>
2016-10-21 11:42:58 +0100

    set up firebase

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import {createStore } from 'redux';
 import reducers from './redux';
+import firebase from 'firebase';
 
 export default class App extends Component {
+  componentWillMount() {
+    // Initialize Firebase - it can only run one time!
+     const config = {
+       apiKey: 'AIzaSyA-YUVxpJDe6ESBqE0h18MJsuaah-O2OKQ',
+       authDomain: 'authexample-3354a.firebaseapp.com',
+       databaseURL: 'https://authexample-3354a.firebaseio.com',
+       storageBucket: 'authexample-3354a.appspot.com',
+       messagingSenderId: '293037288767'
+     };
+     firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
